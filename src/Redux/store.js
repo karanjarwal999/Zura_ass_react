408d849cff2d0272b9f1b1fe333ab91882c72602
karanjarwal999 <karanjarwal999@gmail.com>
2023-12-01 12:43:46 +0530

    deleting data

--- src/Redux/store.js
+++
@@ -1,6 +0,0 @@
-import { applyMiddleware, legacy_createStore } from 'redux'
-import Reducer from './Reducer'
-import thunk from 'redux-thunk'
-
-
-export  default  legacy_createStore(Reducer,applyMiddleware(thunk))
